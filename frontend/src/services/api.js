121467664e1952f39377f0c4d3325d15f05a4038
Nitish kumar singh <nkk7367095@gmail.com>
2025-10-21 13:17:39 +0530

    adding api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://fix4ever-brown.vercel.app',
+  baseURL: import.meta.env.VITE_API_URL || 'https://fix4ever-brown.vercel.app/api',
 });
 
 export const setAuthToken = (token) => {
