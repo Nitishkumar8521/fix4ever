2f4b975d6e4f243614c084fd1499dd663b294600
Nitish kumar singh <nkk7367095@gmail.com>
2025-10-21 13:06:39 +0530

    removing the unnecessary file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://fix4ever-brown.vercel.app',
 });
 
 export const setAuthToken = (token) => {
